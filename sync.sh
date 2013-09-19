cb30f885dc71afd95b88485b9c8790353d3fd658
Will Vincent <william.s.vincent@gmail.com>
2013-09-19 10:10:25 -0400

    adding post-commit.sh to sync.sh

--- sync.sh
+++ sync.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
-# git pull origin master
+# git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-GPL.txt" \
-		--exclude "LICENSE-MIT.txt" -av --no-perms . ~
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "post-commit.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 unset doIt
 source ~/.bash_profile
