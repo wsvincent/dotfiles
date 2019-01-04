8f8879a757c8a3485de82f877c22a725b60f6630
William Vincent <will@wsvincent.com>
2019-01-04 09:15:49 -0500

    lower text size

--- .hyper.js
+++ .hyper.js
@@ -2,7 +2,7 @@ module.exports = {
   updateChannel: 'canary',
   config: {
     // default font size in pixels for all tabs
-    fontSize: 20,
+    fontSize: 12,
     windowSize: [1080, 720],
     fontFamily: '"Triplicate", "Inconsolata for Powerline", monospace',
     cursorShape: 'BLOCK',
@@ -15,5 +15,5 @@ module.exports = {
   plugins: [
     'hyperterm-cobalt2-theme',
   ],
-  
+
 };
